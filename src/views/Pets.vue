9fb10da96e94533fa2b91ea06590a907f382a114
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-08-05 11:00:05 -0300

    Updating Pets page

--- src/views/Pets.vue
+++ src/views/Pets.vue
@@ -1,3 +1,27 @@
 <template>
-  <div>Pets</div>
+  <div>
+    <h1>{{ animal.name }} ({{ animal.species }})</h1>
+    <p>Age: {{ animal.age }}</p>
+    <p>Breed: {{ animal.breed }}</p>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data() {
+    return {
+      animal: {}
+    }
+  },
+  computed: {
+    ...mapState(['dogs', 'cats'])
+  },
+  mounted() {
+    const animal = this[this.$route.params.species][this.$route.params.id]
+    const species = this.$route.params.species
+    this.animal = { ...animal, species }
+  }
+}
+</script>
